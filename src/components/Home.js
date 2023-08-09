eeca4c95022a5b0e9342b63db7a06033e01af8c4
husada20 <husadahutasoit@gmail.com>
2023-08-09 23:26:57 +0700

    edit & delete buttton

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,47 +1,56 @@
-import React, { Fragment } from 'react';
-import {  Table } from 'react-bootstrap';
+import React, { Fragment } from "react";
+import { Button, Table } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Employees from './Employees';
+import Employees from "./Employees";
+import {Link, useNavigate} from 'react-router-dom'
 
 function Home() {
-    return (
-        <Fragment>
-            <div style={{ margin: "10rem" }}>
-                <Table striped bordered hover size="sm">
-                    <thead>
-                        <tr>
-                            <th>
-                                Name
-                            </th>
-                            <th>
-                                Age
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            Employees && Employees.length > 0
-                                ? Employees.map((item, index) => (
-                                    <tr key={index}>
-                                        <td>
-                                            {item.Name}
-                                        </td>
-                                        <td>
-                                            {item.Age}
-                                        </td>
-                                    </tr>
-                                ))
-                                : (
-                                    <tr>
-                                        <td colSpan="2">No data available</td>
-                                    </tr>
-                                )
-                        }
-                    </tbody>
-                </Table>
-            </div>
-        </Fragment>
-    );
+
+    let history = useNavigate()
+
+    const handleDelete = (id) => {
+        var index = Employees.map(function(e) {
+            return e.id
+        }).indexOf(id);
+
+        Employees.splice(index, 1);
+
+        history('/')
+
+    }
+  return (
+    <Fragment>
+      <div style={{ margin: "10rem" }}>
+        <Table striped bordered hover size="sm">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Age</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Employees && Employees.length > 0 ? (
+              Employees.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.Name}</td>
+                  <td>{item.Age}</td>
+                  <td>
+                  <Button style={{ marginRight: '10px' }} onClick={() => { alert(item.id) }}>Edit</Button>
+                <Button style={{ marginLeft: '10px' }} onClick={() => { handleDelete(item.id) }}>DELETE</Button>
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan="2">No data available</td>
+              </tr>
+            )}
+          </tbody>
+        </Table>
+      </div>
+    </Fragment>
+  );
 }
 
 export default Home;
